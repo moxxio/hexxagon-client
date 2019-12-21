32a1beed6bbd89f3728ee0a3de8242d424bd9e3d
Johannes Rauch <johannes.rauch@uni-ulm.de>
2019-12-21 15:32:19 +0100

    AbstractMessage: Is now a abstract event for state pattern

--- core/src/de/johannesrauch/hexxagon/network/abstractmessage/AbstractMessage.java
+++ core/src/de/johannesrauch/hexxagon/network/abstractmessage/AbstractMessage.java
@@ -2,9 +2,10 @@ package de.johannesrauch.hexxagon.network.abstractmessage;
 
 import java.util.UUID;
 
+import de.johannesrauch.hexxagon.automaton.actions.AbstractEvent;
 import de.johannesrauch.hexxagon.network.messagetype.MessageType;
 
-public abstract class AbstractMessage {
+public abstract class AbstractMessage extends AbstractEvent {
 	
 	public MessageType messageType;
 	public UUID userId;
