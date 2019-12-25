0558e40c3f48a56d11f2a059e65b52f0166d41ec
Johannes Rauch <johannes.rauch@uni-ulm.de>
2019-12-25 17:24:38 +0100

    BaseScreen: Switched to screen viewport

--- core/src/de/johannesrauch/hexxagon/view/screen/BaseScreen.java
+++ core/src/de/johannesrauch/hexxagon/view/screen/BaseScreen.java
@@ -3,6 +3,7 @@ package de.johannesrauch.hexxagon.view.screen;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import de.johannesrauch.hexxagon.Hexxagon;
 
@@ -11,14 +12,16 @@ public abstract class BaseScreen implements Screen {
     protected Hexxagon parent;
 
     protected OrthographicCamera camera;
-    protected StretchViewport viewport;
+    // protected StretchViewport viewport; // TODO: inform about and choose right viewport
+    protected ScreenViewport viewport;
     protected Stage stage;
 
     public BaseScreen(Hexxagon parent) {
         this.parent = parent;
 
         camera = new OrthographicCamera(1280, 720);
-        viewport = new StretchViewport(1280, 720, camera);
+        // viewport = new StretchViewport(1280, 720, camera);
+        viewport = new ScreenViewport(camera);
         stage = new Stage();
     }
 }
