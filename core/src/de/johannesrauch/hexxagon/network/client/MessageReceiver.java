790ab26ab93e4e75a28eb2591b2d05d5a840dd27
Johannes Rauch <johannes.rauch@uni-ulm.de>
2019-12-28 14:00:29 +0100

    Emitter and Receiver: Added comments; Created setter for lobby and game handler

--- core/src/de/johannesrauch/hexxagon/network/client/MessageReceiver.java
+++ core/src/de/johannesrauch/hexxagon/network/client/MessageReceiver.java
@@ -2,23 +2,45 @@ package de.johannesrauch.hexxagon.network.client;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
-import de.johannesrauch.hexxagon.Hexxagon;
+import de.johannesrauch.hexxagon.controller.handler.LobbyHandler;
+import de.johannesrauch.hexxagon.fsm.context.StateContext;
 import de.johannesrauch.hexxagon.network.message.*;
 import de.johannesrauch.hexxagon.network.messagetype.MessageType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * This class handles all received messages from the websocket client.
+ * It converts the json messages back to java objects.
+ * It triggers the effects the events may have on the finite-state machine of the client.
+ *
+ * @author Dennis Jehle
+ * @author Johannes Rauch
+ */
 public class MessageReceiver {
 
-    private static final Logger logger = LoggerFactory.getLogger(MessageReceiver.class);
-    private final Hexxagon parent;
-    private final Gson gson;
+    private final Logger logger = LoggerFactory.getLogger(MessageReceiver.class);
+    private final Gson gson = new Gson();
 
-    public MessageReceiver(Hexxagon parent) {
-        this.parent = parent;
-        gson = new Gson();
+    private StateContext context;
+    private LobbyHandler lobbyHandler;
+    private MessageEmitter messageEmitter;
+
+    /**
+     * This is the standard constructor which sets the message emitter.
+     *
+     * @param messageEmitter the message emitter
+     */
+    public MessageReceiver(MessageEmitter messageEmitter) {
+        this.messageEmitter = messageEmitter;
     }
 
+    /**
+     * This method handles a message which was received.
+     * It identifies the message type and reconstructs the java objects.
+     *
+     * @param json the received json string
+     */
     public void handleMessage(String json) {
         logger.info("Received: " + json);
         try {
@@ -46,34 +68,102 @@ public class MessageReceiver {
         }
     }
 
+    /**
+     * This method sets the state context which handles the reaction on some of the messages.
+     *
+     * @param context the state context
+     */
+    public void setContext(StateContext context) {
+        this.context = context;
+    }
+
+    /**
+     * This method sets the lobby handler so that the available lobbies can get updated directly.
+     *
+     * @param lobbyHandler the lobby handler
+     */
+    public void setLobbyHandler(LobbyHandler lobbyHandler) {
+        this.lobbyHandler = lobbyHandler;
+    }
+
+    /**
+     * This method calls the state context to inform the finite-state machine about the welcome message.
+     *
+     * @param message the welcome message
+     */
     private void receivedWelcome(WelcomeMessage message) {
-        parent.getContext().reactToReceivedWelcome(message);
+        if (context != null) context.reactToReceivedWelcome(message);
+        else logger.warn("Received welcome but context is null!");
     }
 
+    /**
+     * This method informs the lobby handler about the most recent available lobbies.
+     *
+     * @param message the available lobbies message
+     */
     private void receivedAvailableLobbies(AvailableLobbiesMessage message) {
-        parent.getLobbyHandler().updateLobbies(message.getAvailableLobbies());
+        if (lobbyHandler != null) lobbyHandler.updateLobbies(message.getAvailableLobbies());
+        else logger.warn("Received available lobbies but lobby handler is null!");
     }
 
+    /**
+     * This method advices the message emitter to send a get available lobbies message in order to automatically
+     * refresh the available lobbies after the user created one.
+     *
+     * @param message the lobby created message
+     */
     private void receivedLobbyCreated(LobbyCreatedMessage message) {
-        if (message.getSuccessfullyCreated()) parent.getMessageEmitter().sendGetAvailableLobbiesMessage();
+        if (message.getSuccessfullyCreated()) {
+            if (messageEmitter != null) messageEmitter.sendGetAvailableLobbiesMessage();
+            else logger.warn("Received lobby created but message emitter is null!");
+        }
     }
 
+    /**
+     * This method calls the state context to inform the finite-state machine about the joined lobby message.
+     *
+     * @param message the lobby joined message
+     */
     private void receivedLobbyJoined(LobbyJoinedMessage message) {
-        parent.getContext().reactToReceivedJoinedLobby(message);
+        if (context != null) context.reactToReceivedJoinedLobby(message);
+        else logger.warn("Received lobby joined but context is null!");
     }
 
+    /**
+     * This method calls the state context to inform the finite-state machine about the lobby status message.
+     *
+     * @param message the lobby status message
+     */
     private void receivedLobbyStatus(LobbyStatusMessage message) {
-        parent.getContext().reactToReceivedLobbyStatus(message);
+        if (context != null) context.reactToReceivedLobbyStatus(message);
+        else logger.warn("Received lobby status but context is null!");
     }
 
+    /**
+     * This method calls the state context to inform the finite-state machine about the game started message.
+     *
+     * @param message the game started message
+     */
     private void receivedGameStarted(GameStartedMessage message) {
-        parent.getContext().reactToReceivedGameStarted(message);
+        if (context != null) context.reactToReceivedGameStarted(message);
+        else logger.warn("Received game started but context is null!");
     }
 
+    /**
+     * This method calls the state context to inform the finite-state machine about the game status message.
+     *
+     * @param message the game status message
+     */
     private void receivedGameStatus(GameStatusMessage message) {
-        parent.getContext().reactToReceivedGameStatus(message);
+        if (context != null) context.reactToReceivedGameStatus(message);
+        else logger.warn("Received game status but context is null!");
     }
 
+    /**
+     * This method logs the received strike.
+     *
+     * @param message the strike message
+     */
     private void receivedStrike(StrikeMessage message) {
         logger.warn("Received strike " + message.getStrikeCount() + " of " + message.getMaxStrikeCount());
     }
