eecbb1220f76d9d61c19f96fd8c73104cbb989a0
Johannes Rauch <johannes.rauch@uni-ulm.de>
2019-12-28 13:47:08 +0100

    Board: Added comments

--- core/src/de/johannesrauch/hexxagon/model/board/Board.java
+++ core/src/de/johannesrauch/hexxagon/model/board/Board.java
@@ -6,18 +6,35 @@ import de.johannesrauch.hexxagon.model.tile.TileStateEnum;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * This class represents a state of the game board.
+ */
 public class Board {
 
     private Map<TileEnum, TileStateEnum> tiles;
 
+    /**
+     * This is the standard constructor. It creates the tile map.
+     */
     public Board() {
         this.tiles = new HashMap<>();
     }
 
+    /**
+     * This method returns the tile map.
+     *
+     * @return the tile map
+     */
     public Map<TileEnum, TileStateEnum> getTiles() {
         return tiles;
     }
 
+    /**
+     * This method returns the state of a specific tile.
+     *
+     * @param tile the tile you want the state of
+     * @return the state of the given tile
+     */
     public TileStateEnum getTileState(TileEnum tile) {
         return tiles.get(tile);
     }
