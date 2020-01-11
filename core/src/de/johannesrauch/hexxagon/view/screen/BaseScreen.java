fe6adbd9fbf7f77d7eb49d1288d081ed871471c0
Johannes Rauch <johannes.rauch@uni-ulm.de>
2020-01-11 14:52:57 +0100

    BaseScreen: Deleted unnecessary import

--- core/src/de/johannesrauch/hexxagon/view/screen/BaseScreen.java
+++ core/src/de/johannesrauch/hexxagon/view/screen/BaseScreen.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.badlogic.gdx.utils.viewport.StretchViewport;
 import de.johannesrauch.hexxagon.Hexxagon;
 
 /**
